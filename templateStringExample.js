a880261f1145766c18eada7dad1140e38d0c8a91
Trevor Kipkemboi <trevorngetich03@gmail.com>
2025-05-25 19:01:34 +0300

    feat: example of using template literals

---
+++ templateStringExample.js
@@ -0,0 +1,14 @@
+// Declare a constant variable to store the blog title
+const title = 'Best reads of 2019';
+
+// Declare a constant variable to store the author's name
+const author = 'Mario';
+
+// Declare a constant variable to store the number of likes
+const likes = 30;
+
+// Use a template literal to create a string that includes the above variables
+let result = `The blog called ${title} by ${author} has ${likes} likes.`;
+
+// Output the result to the console
+console.log(result);
