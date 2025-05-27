423c81b8e03dc2ff5459500da05b8c9f10435870
Trevor Kipkemboi <trevorngetich03@gmail.com>
2025-05-27 09:24:37 +0300

    feat: add switch statement to display messages based on grade

---
+++ gradeMessageSwitch.js
@@ -0,0 +1,22 @@
+const grade = 'A';
+
+switch(grade){
+    case 'A':
+        console.log('You got an A!');
+        break;
+
+    case 'B':
+        console.log('You got a B!');
+        break;
+    case 'C':
+        console.log('you got a C!');
+        break;
+    case 'D':
+        console.log('You got a D!');
+        break;
+    case 'E':
+        console.log('You got an E!');
+        break;
+    default:
+        console.log('Not a valid grade');
+}
